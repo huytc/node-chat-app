9a71d73eb436266ffb203254f1fa6057ec6ccd5f
huytc <1611331@hcmut.edu.vn>
2019-01-15 16:38:00 +0700

    Add CSS for chat page

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.on('createMessage', (message, callback) => {
         console.log('New message created:', JSON.stringify(message, undefined, 2));
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        callback();
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
