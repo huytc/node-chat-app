902c04098f86dd27ce0f2be20558bfeea143b10c
huytc <1611331@hcmut.edu.vn>
2019-01-15 10:26:22 +0700

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,18 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected.');
 
-    socket.emit('newMessage', {
-        from: 'huytc',
-        text: 'Hello from server',
-        createdAt: Date.now()
-    });
-
     socket.on('disconnect', () => {
         console.log('User was disconnected.');
     });
 
     socket.on('createMessage', message => {
         console.log('New message created:', JSON.stringify(message, undefined, 2));
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 });
 
