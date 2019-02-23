3a62a24f47c84b1f934151a083dc3c500b87e031
huytc <1611331@hcmut.edu.vn>
2019-02-23 11:44:54 +0700

    beautify

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const http = require('http');
 
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage, generateLocationMessage} = require('./utils/message');
-const {isRealString} = require('./utils/validation');
-const {Users} = require('./utils/users');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
+const { Users } = require('./utils/users');
 
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
@@ -16,56 +16,58 @@ const users = new Users();
 
 app.use(express.static(publicPath));
 
-io.on('connection', socket => {
-    console.log('New user connected.');
+io.on('connection', (socket) => {
+	console.log('New user connected.');
 
-    socket.on('join', (params, callback) => {
-        if (!isRealString(params.name) || !isRealString(params.room)) {
-            return callback('Name and room are required.');
-        }
+	socket.on('join', (params, callback) => {
+		if (!isRealString(params.name) || !isRealString(params.room)) {
+			return callback('Name and room are required.');
+		}
 
-        socket.join(params.room);
-        users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.room);
+		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
 
-        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
 
-        callback();
-    });
+		callback();
+	});
 
-    socket.on('disconnect', () => {
-        let user = users.removeUser(socket.id);
+	socket.on('disconnect', () => {
+		let user = users.removeUser(socket.id);
 
-        if (user) {
-            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
-        }
-    });
+		if (user) {
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+		}
+	});
 
-    socket.on('createMessage', (message, callback) => {
-        let user = users.getUser(socket.id);
+	socket.on('createMessage', (message, callback) => {
+		let user = users.getUser(socket.id);
 
-        if (user && isRealString(message.text)) {
-            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
-        }
+		if (user && isRealString(message.text)) {
+			io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+		}
 
-        callback();
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
-    });
+		callback();
+		// socket.broadcast.emit('newMessage', {
+		//     from: message.from,
+		//     text: message.text,
+		//     createdAt: new Date().getTime()
+		// });
+	});
 
-    socket.on('createLocationMessage', coords => {
-        let user = users.getUser(socket.id);
+	socket.on('createLocationMessage', (coords) => {
+		let user = users.getUser(socket.id);
 
-        io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
-    });
+		io
+			.to(user.room)
+			.emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+	});
 });
 
 server.listen(port, () => {
-    console.log(`Server started on port ${port}.`);
+	console.log(`Server started on port ${port}!`);
 });
